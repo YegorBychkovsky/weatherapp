6809df33ff21ae8716b3762f6d8cc50e1337f336
YegorBychkovsky <106180465+YegorBychkovsky@users.noreply.github.com>
2023-07-24 09:14:27 +0300

    complited app

---
+++ src/utils/constants.js
@@ -0,0 +1,27 @@
+// Шаблон для объекта city
+export const cityTemplate = {
+  base: "",
+  clouds: { all: 0 },
+  cod: 0,
+  coord: { lon: 0, lat: 0 },
+  dt: 0,
+  id: 0,
+  main: {
+    temp: 0,
+    feels_like: 0,
+    temp_min: 0,
+    temp_max: 0,
+    pressure: 0,
+  },
+  name: "",
+  sys: { country: "", sunrise: 0, sunset: 0 },
+  timezone: 0,
+  visibility: 0,
+  weather: {
+    id: 0,
+    main: "",
+    description: "",
+    icon: "",
+  },
+  wind: { speed: 0, deg: 0, gust: 0 },
+};
